a91e68617b2d7cccd10cfb6d32c7ad5dc48cebfd
Boris <toborto@yandex.ru>
2019-03-22 11:39:36 +0300

    work in progress...3

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -2,6 +2,7 @@ module.exports = {
     presets: [["@vue/app",
         {
             //useBuiltIns: false,
+            //corejs: "core-js@2"
         }
     ]],
     "plugins": ["@babel/plugin-transform-modules-commonjs"]
